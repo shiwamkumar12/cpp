d2670c34266bc3cd31f15e98396a12ddb8c4eed9
shiwamkumar12 <shiwamhasija6@gmail.com>
2025-02-28 22:47:21 +0500

    Add files via upload

---
+++ shivam hasija 2.cpp
@@ -0,0 +1,146 @@
+#include<iostream>
+using namespace std;
+int main () {
+	
+//+ Variables and Data Types Exercises
+ //ediptive Questions
+        //Qno1. What is a variable in C++?
+        //Define a variable and explain its purpose in C++.
+
+
+cout<<" Ans1. store the value with the help of data type"<<endl;
+cout<<"example:"<<endl;
+ int  a=6;
+ cout<< a <<endl;
+float y=5.55;
+ cout<< y <<endl;
+ 
+ 
+ 
+  //Qno2. What data type would you use to store an age in C++?
+//Give an example of how to declare and initialize such a variable
+
+cout<<"Ans2.  we can store age in int"<<endl;
+cout<<"example"<<endl;
+int age=18;
+cout<< age <<endl;
+
+
+//Qno3. What does the int data type store?
+//Provide an example.
+
+cout<<"Ans3. int store only integers number"<<endl;
+cout<<"example"<<endl;
+
+ int ab=3.33;
+ cout<< ab <<endl;
+    
+    
+    
+    //Qno4. Explain the difference between float and double data types.
+    cout<<"Ans4. float can hold 6-7 digits after decimal"<<endl; cout<<"double can hold 15-16 digits after decimal"<<endl;
+
+
+//Qno5. What data type would you use to store a character?
+//Provide an example of how to declare and initialize this variable.
+ cout<<"Ans5. char can store only single letter, numbers or symbol"<<endl;
+ cout<<"example"<<endl;
+ char gender= 'M';
+
+ cout<< gender <<endl;
+ 
+ //Qno6. What is the default value of a bool variable in C++?
+//Provide a short example where true and false values are used.
+
+
+
+//Qno7. Declare a variable x of type int and assign it the value of 10.
+ cout<<"Ans7. example"<<endl;
+ int x=10;
+ cout<< x <<endl;
+ 
+
+//Qno8. How do you assign one value to multiple variables in C++?
+//Provide an example using variables a, b, and c.
+
+
+//Qno9. What is the purpose of using the const keyword in C++?
+//Wite a short example where a constant variable is used.
+cout<<"Ans9. if we use the const keyword in a datatype then that variable is locked then we can not modified value of variable"<<endl;
+cout<<"EXAMPLE: const int s = 10; this is locked"<<endl;
+
+
+//Qno10.What is the rule for naming variables in C++?
+//List at least three naming conventions or rules for valid identifiers.
+cout<<"1. we cannot use same variable name"<<endl;
+cout<<"2. we cannot use num/digits in variable name"<<endl;
+cout<<"3. cannot use space in variable name"<<endl;
+
+
+
+
+
+//Qno11. Write a program that declares an integer variable num and prints its value.
+ cout<<"Ans11. it is simple tension nahi lene ka"<<endl;
+ 
+ int number=10987;
+ cout<<  number <<endl;
+ 
+ 
+ 
+ //Qno12. Write a program that calculates and prints the perimeter of a rectangle.
+//Assume the length is 10 and the width is 5.
+
+
+
+
+//Qno13. What happens if you try to change the value of a constant variable?
+// Write a program that demonstrates this.
+
+//Qno14.create  that uses a string variable to store your name and prints it.
+
+string mynameis ="shivam kumar";
+cout<<"Qno14: my name is:"<<mynameis<<endl;
+
+
+
+ //Given the following data, write a program to calculate the total fee of a student:
+  
+//Tuition Fee: 5000.75
+//Hostel Fee: 1000.50
+//Books Fee: 300.20
+
+float tuitionfee = 5000.75;
+
+float hostelfee = 1000.50;
+
+float booksfee = 300.20;
+
+float totalfee = tuitionfee+hostelfee+booksfee;
+cout<<"Ans15. total fees:"<<totalfee<<endl;
+
+
+
+
+
+
+ 
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

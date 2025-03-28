742b3e4dbdc8e44ad48fb801e7dcdd1db1a97576
shiwamkumar12 <shiwamhasija6@gmail.com>
2025-03-29 01:39:52 +0500

    Add files via upload

---
+++ control structure -1.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+using namespace std;
+int main () {
+
+
+// Qno1. Greater Than: Write a program to check if a number is greater than 50.
+
+int x = 50;
+int y = 45;
+if(50 > 45) {
+	cout<<"greater than"<<endl;
+}
+//Qno2.Less Than: Write a program to check if a number is less than 100.
+
+int s = 100;
+int h = 200;
+if(100 < 200) {
+	cout<<"less than"<<endl;
+}
+//Qno3.Equal To: Write a program to check if a number is equal to 10.
+int k = 10;
+int l = 10;
+if(10 == 10) {
+	cout<<"eual to"<<endl;
+	}
+
+//Qno4.Not Equal To: Write a program to check if a number is not equal to 0.
+
+int u = 0;
+int o = 0;
+if ( 0 !=0 ) {
+	cout<<"noyt equal to"<<endl;
+}
+//Qno5. Greater Than or Equal To: Write a program to check if a number is greater than or equal to 75.
+
+
+
+
+
+
+//Qno6. Less Than or Equal To: Write a program to check if a number is less than or equal to 25.
+
+
+//Qno7. Age Check: Write a program to check if a person's age is exactly 18.
+
+int age = 18;
+cout<<"18"<<endl;
+cin>> age;
+
+
+if ( age==18) {
+	cout<<"you are 18 year old"<<endl;
+	
+	
+}
+
+else {
+	cout<<"you are not 18 year old"<<endl;
+}
+//Qno8. Temperature Check: Write a program to check if the temperature is below freezing (0°C).
+//Qno9. Score Check: Write a program to check if a student's score is exactly 100.
+
+int score = 90;
+cout<<"score"<<endl;
+cin >> score;
+if( 100==90) {
+	cout<<"student score is 100"<<endl;
+	else  { 
+	cout<<"student score is not 100"<<endl;
+	}
+}
+//Qno10.Negative Number Check: Write a program to check if a number is less than 0.
+
+
+int t = -1;
+cout<<"t"<<endl;
+cin >> t
+if ( -1 < 0)  { 
+cout"num is less than 0"<<endl;
+
+}
+else {
+cout"num is not less than 0"<<endl;	
+}
+//Vowel: Write a program to check if a character is a vowel.
+
+
+
+
+
+
+
+
+}
